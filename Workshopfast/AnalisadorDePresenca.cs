4c777da89dfb95fd249759aca74b82997a250e62
Caio Victor <caiopvasconcelos@gmail.com>
2021-10-29 23:58:41 -0300

    Adicionar arquivos de projeto.

---
+++ Workshopfast/AnalisadorDePresenca.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Workshopfast
+{
+    public class AnalisadorDePresenca
+    {
+        string[] atas;
+        //construtor
+        public AnalisadorDePresenca(string[] atas)
+        {
+            this.atas = atas;
+        }
+
+        public string[] ColaboradoresQueViram2WorkshopsSeguidos()
+        {
+            List<string> listaFinal = new List<string>();
+            List<string> listaAnterior = new List<string>();
+            for (int i = 0; i < atas.Length; i++)
+            {
+                if (i != 0)
+                {
+                    var listaAtual = atas[i].Split(" ");
+                    for (int a = 0; a < listaAtual.Length; a++)
+                    {
+                        if (listaAnterior.Contains(listaAtual[a]))
+                        {
+                            listaFinal.Add(listaAtual[a]);
+                        }
+
+                    }
+                }
+                listaAnterior = atas[i].Split(" ").ToList();
+            }
+            return listaFinal.Distinct().OrderBy(x => x).ToArray();
+        }
+    }
+
+}
---
+++ Workshopfast/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Workshopfast
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            int workshop;
+            Console.WriteLine("Quantos workshop quer verificar ?");
+            workshop = int.Parse(Console.ReadLine());
+
+            string[] atas = new string [workshop];
+            for (int i = 0; i < atas.Length; i++)
+            {
+                Console.WriteLine($"Digite os identificadores das pessoas que participaram do {i+1} WorkShop");
+                atas[i] = Console.ReadLine();
+            }
+            AnalisadorDePresenca analisador = new AnalisadorDePresenca(atas);
+            var listaFinal = analisador.ColaboradoresQueViram2WorkshopsSeguidos(); 
+            foreach(var a in listaFinal)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
